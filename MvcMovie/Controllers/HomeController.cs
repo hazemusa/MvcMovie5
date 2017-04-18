45cb1bdd0defc61c444fb0b368903256e0de3644
Hazem Farra <faha1301@stcloudstate.edu>
2017-04-18 00:40:34 -0500

    Added MvcApplication Project

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using MvcMovie.Models;
 using MvcMovieApplication.Customer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 
---
+++ MvcMovieApplication/Customer/CustomerApplicationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcMovieApplication.Customer
+{
+    public class CustomerApplicationService
+    {
+        public CustomerApplicationService()
+        {
+
+        }
+
+        public long GetCustomerFromGuid(Guid guid)
+        {
+            return 1;
+        }
+    }
+}
